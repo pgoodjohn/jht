1449d0476f2a02c09055db3470fc5934415eb138
Pietro Bongiovanni <info@pietrobongiovanni.com>
2022-07-25 18:07:59 +0200

    adds cargo support, moves build to separate command and introduces more empty commands

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,122 @@
+use clap::{Parser, Subcommand};
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
+
+/// Next steps:
+/// - Init Script (creates index.html, listing.html, config.toml)
+/// - Config reading
+/// - Styles
+///
+
+#[derive(Parser)]
+#[clap(version, about, arg_required_else_help(true))]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    #[clap(short, long, global = true)]
+    debug: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialise Configuration
+    Init,
+    /// Build your website
+    Build,
+    /// Manage your configuration
+    Config,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Commands::Init) => {
+            init_command();
+        }
+        Some(Commands::Build) => {
+            build_command();
+        }
+        Some(Commands::Config) => {
+            config_command();
+        }
+        None => {
+            failure_message();
+        }
+    }
+
+    return;
+}
+
+fn build_command() {
+    println!("Build command");
+    // Get a config object as parameter
+
+    // Build index.html
+    build_index().expect("Could not build index.html");
+
+    // Build content pages
+    let content_list = build_content_pages(std::path::Path::new("./templates/article.html"))
+        .expect("Could not build content");
+
+    // Build content list page
+    build_listing_page(content_list).expect("Could not build listing page");
+
+    // Build other pages
+}
+
+fn init_command() {
+    println!("Init command");
+}
+
+fn config_command() {
+    println!("Config command");
+}
+
+fn failure_message() {
+    println!("No command given");
+}
+
+fn build_index() -> Result<(), ()> {
+    println!("Creating index.html from templates.index.html");
+    let index_template = std::fs::read_to_string(std::path::Path::new("./templates/index.html"))
+        .expect("home templates missing");
+
+    let mut new_index_page = File::create(std::path::Path::new("./build/index.html"))
+        .expect("unable to create new listing page");
+
+    new_index_page
+        .write_all(index_template.as_bytes())
+        .expect("could not create new index");
+    Ok(())
+}
+
+struct ContentList {
+    items: Vec<String>,
+}
+
+fn build_content_pages(content_page_template: &Path) -> Result<ContentList, ()> {
+    println!(
+        "Building content pages with template {}",
+        content_page_template.as_os_str().to_str().unwrap()
+    );
 
     let all_content =
         std::fs::read_dir(std::path::Path::new("./content")).expect("could not find content dir");
 
-    println!("{:?}", all_content);
+    // todo!("Create a folder for the content it it doesn't already exist or it will crash");
 
     let mut all_articles = Vec::new();
 
     for entry in all_content.into_iter() {
-        println!("{:?}", entry);
-
         let unwrapped = entry.unwrap();
 
         let file_contents = std::fs::read_to_string(unwrapped.path()).expect("unable to read file");
 
         let article_template =
-            std::fs::read_to_string(std::path::Path::new("./templates/article.html"))
-                .expect("article template missing");
+            std::fs::read_to_string(content_page_template).expect("article template missing");
 
         let article_page = article_template.replace("{article}", &file_contents);
 
@@ -34,8 +130,8 @@ fn main() {
                 .expect("bigger fail")
         ));
 
-        let mut new_file =
-            File::create(std::path::Path::new(&new_file_name)).expect("failed to create a file");
+        let mut new_file = File::create(std::path::Path::new(&new_file_name))
+            .expect("failed to create a file to store the article");
 
         new_file
             .write_all(article_page.as_bytes())
@@ -44,9 +140,19 @@ fn main() {
         all_articles.push(new_file_name)
     }
 
+    println!("Build content pages {:?}", all_articles);
+
+    Ok(ContentList {
+        items: all_articles,
+    })
+}
+
+fn build_listing_page(content_list: ContentList) -> Result<(), ()> {
+    println!("Adding {:?} to listing page", content_list.items);
+
     let mut article_hrefs = String::new();
 
-    for article in all_articles {
+    for article in content_list.items {
         article_hrefs.push_str(&format!(
             "<a href={}>{}</a> <br />",
             article.strip_prefix("./build/").expect("big bad"),
@@ -66,13 +172,5 @@ fn main() {
         .write_all(list_page.as_bytes())
         .expect("could not create new listing file");
 
-    let index_template = std::fs::read_to_string(std::path::Path::new("./templates/index.html"))
-        .expect("home templates missing");
-
-    let mut new_index_page = File::create(std::path::Path::new("./build/index.html"))
-        .expect("unable to create new listing page");
-
-    new_index_page
-        .write_all(index_template.as_bytes())
-        .expect("could not create new index");
+    Ok(())
 }
