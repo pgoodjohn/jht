6351e23eace4729d64f05eef316915ecb562ca68
Pietro Bongiovanni <info@pietrobongiovanni.com>
2022-07-28 07:59:48 +0200

    create gitignore file ignoring the binary on jht init

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use super::configuration;
 use crate::ConfigurationFilePath;
 use clap::Parser;
@@ -22,6 +24,12 @@ impl super::ConfigurationFilePath for InitCommand {
 pub fn command(init_command: &InitCommand) {
     log::info!("Running init command");
 
+    // TODO: More things that this command could do
+    // - Create git repository
+    // - Create simple templates for
+    //      - index.html
+    //      - content.html
+    //      - blog.html
     let configuration_file_path = init_command.path();
 
     log::info!(
@@ -29,14 +37,38 @@ pub fn command(init_command: &InitCommand) {
         configuration_file_path
     );
 
-    // Create configuration
     let configuration = configuration::Config::init(configuration_file_path);
-    // Create folder for content
-    std::fs::create_dir_all(std::path::Path::new(
-        &configuration.build_config.build_directory,
-    ))
-    .expect("Failed to create build directory");
-    // Create folder for templates
-    std::fs::create_dir_all(std::path::Path::new(&configuration.templates_directory))
+    create_content_directory(&configuration);
+    creaet_templates_directory(&configuration);
+
+    create_gitignore();
+}
+
+fn create_content_directory(config: &configuration::Config) {
+    std::fs::create_dir_all(std::path::Path::new(&config.content_dir))
+        .expect("Failed to create content directory");
+}
+
+fn creaet_templates_directory(config: &configuration::Config) {
+    std::fs::create_dir_all(std::path::Path::new(&config.templates_directory))
         .expect("Failed to create build directory");
+}
+
+fn create_gitignore() {
+    let mut gitignore_path = std::path::PathBuf::new();
+    gitignore_path.push(".");
+    gitignore_path.push(".gitgnore");
+
+    if gitignore_path.exists() {
+        log::debug!("Found .gitignore file, skipping creation.");
+        return;
+    }
+
+    let gitignore_contents = String::from("just-html");
+
+    let mut gitignore_file =
+        std::fs::File::create(&gitignore_path).expect("failed to create a .gitignore file");
+    gitignore_file
+        .write_all(gitignore_contents.as_bytes())
+        .expect("failed to write to .gitignore file")
 }
